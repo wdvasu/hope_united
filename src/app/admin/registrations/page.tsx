c63e0e9bb8b94212462f84d37f147e8abd4ed00b
wdvasu <bill@cybermanageit.com>
2025-11-17 22:49:19 -0600

    feat(admin): show all registration fields on recent registrations table

--- src/app/admin/registrations/page.tsx
+++ src/app/admin/registrations/page.tsx
@@ -94,8 +94,22 @@ export default async function AdminRegistrationsPage({ searchParams }: { searchP
               <th className="text-left p-2">ZIP</th>
               <th className="text-left p-2">Veteran</th>
               <th className="text-left p-2">Drugs</th>
+              <th className="text-left p-2">Drug Other</th>
+              <th className="text-left p-2">Sexual Orientation</th>
+              <th className="text-left p-2">Sexual Other</th>
               <th className="text-left p-2">Gender</th>
+              <th className="text-left p-2">Gender Other</th>
+              <th className="text-left p-2">Race</th>
+              <th className="text-left p-2">Race Other</th>
+              <th className="text-left p-2">Ethnicity</th>
               <th className="text-left p-2">County</th>
+              <th className="text-left p-2">County Other</th>
+              <th className="text-left p-2">Waiver Agreed</th>
+              <th className="text-left p-2">E‑Sign Name</th>
+              <th className="text-left p-2">E‑Sign At</th>
+              <th className="text-left p-2">Device ID</th>
+              <th className="text-left p-2">Created IP</th>
+              <th className="text-left p-2">User Agent</th>
             </tr>
           </thead>
           <tbody>
@@ -107,8 +121,22 @@ export default async function AdminRegistrationsPage({ searchParams }: { searchP
                 <td className="p-2">{r.zipCode}</td>
                 <td className="p-2">{r.veteranStatus}</td>
                 <td className="p-2">{(r.drugs || []).join(', ')}</td>
+                <td className="p-2">{r.drugOther || ''}</td>
+                <td className="p-2">{r.sexualOrientation}</td>
+                <td className="p-2">{r.sexualOther || ''}</td>
                 <td className="p-2">{r.gender}</td>
+                <td className="p-2">{r.genderOther || ''}</td>
+                <td className="p-2">{r.race}</td>
+                <td className="p-2">{r.raceOther || ''}</td>
+                <td className="p-2">{r.ethnicity}</td>
                 <td className="p-2">{r.county}</td>
+                <td className="p-2">{r.countyOther || ''}</td>
+                <td className="p-2">{r.waiverAgreed ? 'Yes' : 'No'}</td>
+                <td className="p-2">{r.eSignatureName}</td>
+                <td className="p-2">{new Date(r.eSignatureAt).toLocaleString()}</td>
+                <td className="p-2 font-mono">{r.deviceId}</td>
+                <td className="p-2">{r.createdIp || ''}</td>
+                <td className="p-2 max-w-[320px] whitespace-pre-wrap break-words text-xs">{r.userAgent || ''}</td>
               </tr>
             ))}
           </tbody>
